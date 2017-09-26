34c113d2e0eb24043ccce3b064e1bc0317c53767
Andy Wickert <andy@northernwidget.com>
2017-09-26 17:55:14 -0500

    Works top to bottom

--- workflow.sh
+++ workflow.sh
@@ -13,6 +13,9 @@ slope=slope_tmp
 aspect=aspect_tmp
 HRUs=HRUs_tmp
 gravity_reservoirs=gravity_reservoirs_tmp
+basin_mask=basin_mask_tmp
+basin_mask_out=basin_mask
+pour_point=pp_tmp
 
 # Set region
 g.region -p rast=$DEM
@@ -35,13 +38,13 @@ v.stream.network map=$streams
 
 # Restrict to a single basin
 basin_outlet_cat=144 # You must find and define this after building the stream network
-v.stream.inbasin input_streams=$streams input_basins=$basins output_streams=$streams_onebasin output_basin=$basins_onebasin cat=$basin_outlet_cat --o
+v.stream.inbasin input_streams=$streams input_basins=$basins output_streams=$streams_onebasin output_basin=$basins_onebasin cat=$basin_outlet_cat output_pour_point=$pour_point --o
 
 # GSFLOW segments: sections of stream that define subbasins
 v.gsflow.segments input=$streams_onebasin output=$segments --o
 
-# MODFLOW grid
-v.gsflow.grid basin=$basins_onebasin dx=150 dy=150 output=$grid --o
+# MODFLOW grid & basin mask (1s where basin exists and 0 where it doesn't)
+v.gsflow.grid basin=$basins_onebasin dx=150 dy=150 output=$grid mask_output=$basin_mask pour_point=$pour_point --o
 
 # GSFLOW reaches: intersection of segments and grid
 v.gsflow.reaches segment_input=$segments grid_input=$grid elevation=$DEM output=$reaches --o
@@ -52,4 +55,10 @@ v.gsflow.hruparams input=$basins_onebasin output=$HRUs slope=$slope aspect=$aspe
 
 # GSFLOW gravity reservoirs
 v.gsflow.gravres hru_input=$HRUs grid_input=$grid output=$gravity_reservoirs --o
+
+# Export basin mask -- 1s where basin exists and 0 where it doesn't
+r.out.ascii input=$basin_mask output=$basin_mask_out.asc null_value=0
+
+# Export tables and discharge point
+v.gsflow.export reaches_input=$reaches segments_input=$segments gravres_input=$gravity_reservoirs pour_point_input=$pour_point reaches_output=$reaches segments_output=$segments gravres_output=$gravity_reservoirs pour_point_output=$pour_point --o
 
