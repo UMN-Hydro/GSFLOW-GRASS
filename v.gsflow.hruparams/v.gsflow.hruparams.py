136d5e3d889c9bcf73f0ffe5b0e8a5d41dcec7d5
Andy Wickert <andy@northernwidget.com>
2017-09-26 12:38:38 -0500

    works to populate HRU database

--- v.gsflow.hruparams/v.gsflow.hruparams.py
+++ v.gsflow.hruparams/v.gsflow.hruparams.py
@@ -17,110 +17,45 @@
 #
 # REQUIREMENTS:
 #      -  uses inputs from v.stream.network
- 
+#      -  uses inputs from r.slope.aspect
+
 # More information
 # Started December 2016
 
 #%module
-#% description: Set parameters for GSFLOW Hydrologic Response Units (HRUs)
-#% keyword: vector
-#% keyword: stream network
-#% keyword: hydrology
-#% keyword: GSFLOW
+#%  description: Set parameters for GSFLOW Hydrologic Response Units (HRUs)
+#%  keyword: vector
+#%  keyword: stream network
+#%  keyword: hydrology
+#%  keyword: GSFLOW
 #%end
 
 #%option G_OPT_V_INPUT
 #%  key: input
-#%  label: Vector stream network from r.stream.extract
+#%  label: Sub-basins to become HRUs
 #%  required: yes
 #%  guidependency: layer,column
 #%end
 
 #%option G_OPT_V_OUTPUT
 #%  key: output
-#%  label: Segments: stream segments for GSFLOW / PRMS
+#%  label: HRUs
 #%  required: yes
 #%  guidependency: layer,column
 #%end
 
 #%option
-#%  key: ICALC
-#%  type: integer
-#%  description: Stream depth option: 0-const; 1,2-Manning; 3-aQ^b
-#%  answer: 3
-#%  required: no
-#%end
-
-#%option
-#%  key: CDPTH
-#%  type: double
-#%  description: Flow depth coefficient; used if ICALC=3
-#%  answer: 0.25
-#%  required: no
-#%end
-
-#%option
-#%  key: FDPTH
-#%  type: double
-#%  description: Flow depth exponent; used if ICALC=3
-#%  answer: 0.4
-#%  required: no
-#%end
-
-#%option
-#%  key: AWDTH
-#%  type: double
-#%  description: Flow width coefficient; used if ICALC=3
-#%  answer: 8
-#%  required: no
-#%end
-
-#%option
-#%  key: BWDTH
-#%  type: double
-#%  description: Flow width exponent; used if ICALC=3
-#%  answer: 0.5
-#%  required: no
-#%end
-
-#%option
-#%  key: IUPSEG
+#%  key: slope
 #%  type: string
-#%  description: Category of upstream diversion segment (from_cat,to_cat,...)
-#%  answer: 0,0
-#%  required: no
+#%  description: Slope [unitless]: r.slope.aspect format=percent zscale=0.01
+#%  required: yes
 #%end
 
 #%option
-#%  key: FLOW
+#%  key: aspect
 #%  type: string
-#%  description: Streamflow entering the upstream-most segments (cat,Q,cat,Q,...)
-#%  answer: 0,0
-#%  required: no
-#%end
-
-#%option
-#%  key: RUNOFF
-#%  type: string
-#%  description: Diffuse runoff entering each segment (cat,Q,cat,Q,...)
-#%  answer: 0,0
-#%  required: no
-#%end
-
-#%option
-#%  key: ETSW
-#%  type: string
-#%  description: Direct removal of in-channel water by ET (cat,Q,cat,Q)
-#%  answer: 0,0
-#%  required: no
-#%end
-
-#%option
-#%  key: PPTSW
-#%  type: string
-#%  description: Direct precipitation on the stream
-#%  answer: 0,0
-#%  required: no
+#%  description: Aspect from r.slope.aspect
+#%  required: yes
 #%end
 
 ##################
@@ -152,13 +87,25 @@ def main():
     Adds GSFLOW parameters to a set of HRU sub-basins
     """
 
-    #########################################
-    # Attributes (in order given in manual) #
-    #########################################
+    ##################
+    # OPTION PARSING #
+    ##################
 
+    options, flags = gscript.parser()
+    basins = options['input']
+    HRU = options['output']
+    slope = options['slope']
+    aspect = options['aspect']
 
-    # COLUMNS
-    ##########
+    ################################
+    # CREATE HRUs FROM SUB-BASINS  #
+    ################################
+
+    g.copy(vector=(basins,HRU), overwrite=gscript.overwrite())
+
+    ############################################
+    # ATTRIBUTE COLUMNS (IN ORDER FROM MANUAL) #
+    ############################################
 
     # HRU
     hru_columns = []
@@ -200,22 +147,16 @@ def main():
     hru_columns.append('obsin_segment integer') # Index of measured streamflow station that replaces
                                                 #   inflow to a segment
 
-
-    # PRODUCE THE DATA TABLES
-    ##########################
-
     # Create strings
     hru_columns = ",".join(hru_columns)
-
-    # Copy to create a HRU file
-    g.copy(vector=(basins,HRU), overwrite=grass.overwrite())
 
     # Add columns to tables
     v.db_addcolumn(map=HRU, columns=hru_columns, quiet=True)
 
 
-    # Produce the data table entries
-    ##################################
+    ###########################
+    # UPDATE DATABASE ENTRIES #
+    ###########################
 
     colNames = np.array(gscript.vector_db_select(HRU, layer=1)['columns'])
     colValues = np.array(gscript.vector_db_select(HRU, layer=1)['values'].values())
@@ -240,39 +181,44 @@ def main():
     # Close the table
     hru.close()
 
+    """
     # Do the same for basins <-------------- DO THIS OR SIMPLY HAVE HRUs OVERLAIN WITH GRID CELLS? IN THIS CASE, RMV AREA ADDITION TO GRAVRES
-    v.db_addcolumn(map=basins, columns='id int')
+    v.db_addcolumn(map=basins, columns='id int', quiet=True)
     basins = VectorTopo(basins)
     basins.open('rw')
     cur = basins.table.conn.cursor()
     cur.executemany("update basins set id=? where cat=?", nhrut)
     basins.table.conn.commit()
     basins.close()
+    """
 
     # if you want to append to table
     # cur.executemany("update HRU(id) values(?)", nhrut) # "insert into" will add rows
 
     #hru_columns.append('hru_area double precision')
     # Acres b/c USGS
-    v.to_db(map=HRU, option='area', columns='hru_area', units='acres')
+    v.to_db(map=HRU, option='area', columns='hru_area', units='acres', quiet=True)
+
 
     # GET MEAN VALUES FOR THESE NEXT ONES, ACROSS THE BASIN
-    # Slope
-    r.slope_aspect(elevation='srtm', slope='slope', aspect='aspect', format='percent', zscale=0.01, overwrite=True) # zscale=0.01 to make percent be decimal
-    #grass.mapcalc('slope = tmp / 100.', overwrite=True)
-    v.rast_stats(map=HRU, raster='slope', method='average', column_prefix='tmp', flags='c')
-    v.db_update(map=HRU, column='hru_slope', query_column='tmp_average')
-    v.db_dropcolumn(map=HRU, columns='tmp_average')
+
+    # SLOPE (and aspect) 
+    #####################
+    v.rast_stats(map=HRU, raster='slope', method='average', column_prefix='tmp', flags='c', quiet=True)
+    v.db_update(map=HRU, column='hru_slope', query_column='tmp_average', quiet=True)
+
+    # ASPECT
+    #########
+    v.db_dropcolumn(map=HRU, columns='tmp_average', quiet=True)
     # Dealing with conversion from degrees (no good average) to something I can
     # average -- x- and y-vectors
     # Geographic coordinates, so sin=x, cos=y.... not that it matters so long 
     # as I am consistent in how I return to degrees
-    r.mapcalc('aspect_x = sin(aspect)', overwrite=True)
-    r.mapcalc('aspect_y = cos(aspect)', overwrite=True)
+    r.mapcalc('aspect_x = sin(aspect)', overwrite=True, quiet=True)
+    r.mapcalc('aspect_y = cos(aspect)', overwrite=True, quiet=True)
     #grass.run_command('v.db.addcolumn', map=HRU, columns='aspect_x_sum double precision, aspect_y_sum double precision, ncells_in_hru integer')
-    v.rast_stats(map=HRU, raster='aspect_x', method='sum', column_prefix='aspect_x', flags='c')
-    v.rast_stats(map=HRU, raster='aspect_y', method='sum', column_prefix='aspect_y', flags='c')
-
+    v.rast_stats(map=HRU, raster='aspect_x', method='sum', column_prefix='aspect_x', flags='c', quiet=True)
+    v.rast_stats(map=HRU, raster='aspect_y', method='sum', column_prefix='aspect_y', flags='c', quiet=True)
     hru = VectorTopo(HRU)
     hru.open('rw')
     cur = hru.table.conn.cursor()
@@ -288,10 +234,14 @@ def main():
     hru.table.conn.commit()
     hru.close()
 
-    # hru_columns.append('hru_elev double precision') # Mean elevation
-    v.rast_stats(map=HRU, raster='srtm', method='average', column_prefix='tmp', flags='c')
-    v.db_update(map=HRU, column='hru_elev', query_column='tmp_average')
-    v.db_dropcolumn(map=HRU, columns='tmp_average')
+    # ELEVATION
+    ############
+    v.rast_stats(map=HRU, raster='srtm', method='average', column_prefix='tmp', flags='c', quiet=True)
+    v.db_update(map=HRU, column='hru_elev', query_column='tmp_average', quiet=True)
+    v.db_dropcolumn(map=HRU, columns='tmp_average', quiet=True)
+
+    # CENTROIDS -- NEED FIXING FOR TRUE CENTERS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ############
 
     # get x,y of centroid -- but have areas not in database table, that do have
     # centroids, and having a hard time finding a good way to get rid of them!
@@ -301,15 +251,15 @@ def main():
     # From looking at map, lots of extra centroids on area boundaries, and removing
     # small areas (though threshold hard to guess) gets rid of these
 
-    v.db_addcolumn(map=HRU, columns='centroid_x double precision, centroid_y double precision')
-    v.to_db(map=HRU, type='centroid', columns='centroid_x, centroid_y', option='coor', units='meters')
+    v.db_addcolumn(map=HRU, columns='centroid_x double precision, centroid_y double precision', quiet=True)
+    v.to_db(map=HRU, type='centroid', columns='centroid_x, centroid_y', option='coor', units='meters', quiet=True)
 
     # hru_columns.append('hru_lat double precision') # Latitude of centroid
     colNames = np.array(gscript.vector_db_select(HRU, layer=1)['columns'])
     colValues = np.array(gscript.vector_db_select(HRU, layer=1)['values'].values())
     xy = colValues[:,(colNames=='centroid_x') + (colNames=='centroid_y')]
     np.savetxt('_xy.txt', xy, delimiter='|', fmt='%s')
-    m.proj(flags='od', input='_xy.txt', output='_lonlat.txt', overwrite=True)
+    m.proj(flags='od', input='_xy.txt', output='_lonlat.txt', overwrite=True, quiet=True)
     lonlat = np.genfromtxt('_lonlat.txt', delimiter='|',)[:,:2]
     lonlat_cat = np.concatenate((lonlat, np.expand_dims(_cat, 2)), axis=1)
 
@@ -323,17 +273,20 @@ def main():
     hru.table.conn.commit()
     hru.close()
 
-    # FIX!!!
+    # FIX!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # CENTROIDS ARE NOT REALLY CENTERS, AND DO NOT WORK IF THERE ARE MULTIPLE
     # AREAS
     # Pygrass to find area edges / cells, and get the center?
     # https://grass.osgeo.org/grass70/manuals/libpython/pygrass.vector.html
 
     # Easting and Northing for other columns
-    v.db_update(map=HRU, column='hru_x', query_column='centroid_x')
-    v.db_update(map=HRU, column='hru_xlong', query_column='centroid_x*3.28084') # feet
-    v.db_update(map=HRU, column='hru_y', query_column='centroid_y')
-    v.db_update(map=HRU, column='hru_ylat', query_column='centroid_y*3.28084') # feet
+    v.db_update(map=HRU, column='hru_x', query_column='centroid_x', quiet=True)
+    v.db_update(map=HRU, column='hru_xlong', query_column='centroid_x*3.28084', quiet=True) # feet
+    v.db_update(map=HRU, column='hru_y', query_column='centroid_y', quiet=True)
+    v.db_update(map=HRU, column='hru_ylat', query_column='centroid_y*3.28084', quiet=True) # feet
+
+    # ID NUMBER
+    ############
 
     """
     hru = VectorTopo(HRU)
@@ -344,20 +297,8 @@ def main():
     hru.close()
     """
     # Segment number = HRU ID number
-    v.db_update(map=HRU, column='hru_segment', query_column='id')
+    v.db_update(map=HRU, column='hru_segment', query_column='id', quiet=True)
 
-    #import sys
-    #sys.stdout = open('HRU.csv', 'w')
-    #v.db_select(map=HRU, separator='comma')# > HRU.csv
-    import os
-    os.system('v.db.select map=HRU sep=comma > HRU.csv')
-    # and then sort by id, manually
-    # And then manually change the last segment's "tosegment" to 0.
-    # Except in this case, it was 0!
-    # Maybe I managed to do this automatically above... but tired and late, 
-    # so will check later
-    # but hoping I did something right by re-doing all of the above before
-    # saving (and doing so inside this smaller basin)
-
-
+if __name__ == "__main__":
+    main()
 
