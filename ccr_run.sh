76ef2e0e6fe217740b658270c64a570fe2a4ae4b
ccr <ccr@>
2017-09-30 16:49:58 -0700

    Several modifications to code
    With these modifications, I was able to run GSFLOW successfully on the
    Santa Rosa Island dataset.
    
    in ccr_run.sh:
    -- added several variables to the config file (settings.ini):
       -- proj_name
       -- control_dir
       -- PRMSinput_dir
       -- MODFLOWinput_dir
       -- PRMSoutput_dir
       -- DEM
       These variables are then read by the new python script, settings.py,
       which allows them to be imported as python variables in all of the
       subsequent python scripts.  A new variable called PROJ_CODE, currently
       the space-stripped version of proj_name, is used to prefix file names.
    
    in GSFLOW_print_PRMSparamsfile.py
    -- parsing the basin mask file resulted in errors when converting boundary
       coordinates to numeric types when using lat/long coordinate system, but
       only 'rows' and 'cols' are used.  Fixed this by parsing only if these
       variables are read.
    
    The empty __init__.py is required inside python_scripts/MODFLOW_scripts/
    so that print_MODFLOW_inputs_res_NWT.py can import MODFLOW_NTW_lib.py.
    
    in MODFLOW_NWT_lib.py:
    -- changed functions to accept dischargept_fil as an argument and to read the
       discharge point from this file:
       -- make_uzf3_f_2()
       -- write_ba6_MOD3_2()
    
    -- in write_ba6_MOD3_2(), removed hard-coded IBOUND values
    
    -- Constant hydraulic conductivity was assigned because there was no stream
       buffer file.
    
    -- in make_uzf2_f_2() and write_ba6_MOD3_2(), when discharge points were
       located at the grid boundary, this would result in an index error when
       accessing IBOUND.  Fixed this problem by adding boundary check.
    
    The script print_MODFLOW_inputs_res_NWT.py was moved into its parent
    directory python_scripts/ so that it can import settings.py without
    modifying PYTHONPATH.
    
    in print_MODFLOW_inputs_res_NWT.py:
    -- changed calls to functions from MODFLOW_NWT_lib.py to accept
       dischargept_fil as an argument:
       -- mf.make_uzf3_f_2()
       -- mf.write_ba6_MOD3_2()

--- ccr_run.sh
+++ ccr_run.sh
@@ -1,54 +1,49 @@
 #!/bin/bash
 
-#set -vx
+#gsflow_exe=$HOME"/opt/GSFLOW_1.2.1\ Linux/bin"
+gsflow_exe=$HOME"/opt/GSFLOW-1.2.0/bin"
 
-# (3)
-# this needs to be set by the user
-gsflow_exe=$HOME"/opt/GSFLOW_1.2.1 Linux/bin"
-spin_up="30yr"
+proj_name="SantaRosaIsland"
 
-# (2)
-# in order to fix Permission denied problems after (1)
 local_dir=${PWD}
 
-echo "[settings]" > settings.ini
-echo "local_dir=$local_dir" >> settings.ini
-echo "gsflow_exe=$gsflow_exe" >> settings.ini
-echo "spin_up=$spin_up" >> settings.ini
+settings_ini="$local_dir/settings.ini"
 
 gsflow_dir="$local_dir/GSFLOW"
 
 control_dir="$gsflow_dir/control/"
 PRMSinput_dir="$gsflow_dir/inputs/PRMS/"
 MODFLOWinput_dir="$gsflow_dir/inputs/MODFLOW_NWT/"
-#MODFLOWinput_dir="$gsflow_dir/inputs/MODFLOW_2005/'
 PRMSoutput_dir="$gsflow_dir/outputs/PRMS/"
 
-# directory with files to be read in to generate PRMS input files (to get
-# start/end dates)
-in_data_dir="$gsflow_dir/DataToReadIn/"
+DEM="stros_srtm_3arc_utm"
 
-mkdir -p "$control_dir" "$PRMSinput_dir" "$MODFLOWinput_dir" "$PRMSoutput_dir" "$in_data_dir"
+echo "[settings]" > "$settings_ini"
+echo "proj_name=$proj_name" >> "$settings_ini"
+echo "local_dir=$local_dir" >> "$settings_ini"
+echo "gsflow_exe=$gsflow_exe" >> "$settings_ini"
 
-# (4)
-tar -xvzf Data_GIS.tar.gz
-mv GIS "$in_data_dir/"
+echo "control_dir=$control_dir" >> "$settings_ini"
+echo "PRMSinput_dir=$PRMSinput_dir" >> "$settings_ini"
+echo "MODFLOWinput_dir=$MODFLOWinput_dir" >> "$settings_ini"
+echo "PRMSoutput_dir=$PRMSoutput_dir" >> "$settings_ini"
+echo "DEM=$DEM" >> "$settings_ini"
 
-tar -xvzf GSFLOW_inputs_PRMS_climate.tar.gz
-mv climate "$PRMSinput_dir/"
+mkdir -p "$control_dir" "$PRMSinput_dir" "$MODFLOWinput_dir" "$PRMSoutput_dir"
 
+unzip strosa_data.zip
 
+mkdir -p data/GIS
+cp -R strosa_data/GIS/* data/GIS/
+cp strosa_data/dem/* data/GIS
+cp strosa_data/climate/* "$PRMSinput_dir"
 
-# (1)
-python2.7 "python_scripts/GSFLOW_print_controlfile4_gcng_melt$spin_up.py"
-# tried to run this, needed python-tk
-# sudo apt-get install python-tk
-# then received error
-# "Permission denied: '/home/gcng'"
-# see (2)
+touch python_scripts/MODFLOW_scripts/__init__.py
 
-python2.7 python_scripts/GSFLOW_print_PRMSparamfile4.py
-python2.7 python_scripts/GSFLOW_print_data_climatehru_files1.py
-python2.7 python_scripts/MODFLOW_scripts/print_MODFLOW_inputs_res_NWT.py 
+python2.7 python_scripts/settings.py
+##python2.7 process_weatherdata.py
+python2.7 python_scripts/GSFLOW_print_controlfile.py
+python2.7 python_scripts/GSFLOW_print_PRMSparamfile.py
+python2.7 python_scripts/print_MODFLOW_inputs_res_NWT.py
+./GSFLOW/control/SantaRosaIsland_GSFLOW.sh
 
-"${gsflow_exe}/gsflow" "$gsflow_dir/control/py_ChimTest_Melt_rep${spin_up}_GSFLOW.control" &> out.txt
