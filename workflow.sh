0721c4436597b05f2d01a707578a666e77d62887
Andy Wickert <andy@northernwidget.com>
2017-10-08 12:21:24 -0500

    r.hydrodem

--- workflow.sh
+++ workflow.sh
@@ -1,6 +1,6 @@
-#DEM=srtm_local_filled
+DEM=srtm_local_filled
 #DEM=ASTGTM2_S12W075_dem
-DEM=DEM
+#DEM=DEM
 accumulation=accumulation_tmp
 streams=streams_tmp
 streams_onebasin=${streams}_onebasin
@@ -8,7 +8,8 @@ basins=basins_tmp
 basins_onebasin=${basins}_onebasin
 segments=segments_tmp
 reaches=reaches_tmp
-threshold=1000000 #m2 - drainage area
+#threshold=1000000 #m2 - drainage area
+threshold=100000 #m2 - drainage area
 #grid_res=150 #m2 - for MODFLOW
 grid_res=150 #m2 - for MODFLOW
 grid=grid_tmp
@@ -19,6 +20,7 @@ gravity_reservoirs=gravity_reservoirs_tmp
 basin_mask=basin_mask_tmp
 basin_mask_out=basin_mask
 pour_point=pp_tmp
+icalc=1 # how to compute hydraulic geometry
 
 # Set region
 g.region -p rast=$DEM
@@ -47,11 +49,11 @@ r.to.vect input=$basins output=$basins type=area -v --o
 v.stream.network map=$streams
 
 # Restrict to a single basin
-basin_outlet_cat=2485 #2762 #144 # You must find and define this after building the stream network
+basin_outlet_cat=177 # 144 # 2485 # Lauren #2762 #144 # You must find and define this after building the stream network
 v.stream.inbasin input_streams=$streams input_basins=$basins output_streams=$streams_onebasin output_basin=$basins_onebasin cat=$basin_outlet_cat output_pour_point=$pour_point --o
 
 # GSFLOW segments: sections of stream that define subbasins
-v.gsflow.segments input=$streams_onebasin output=$segments --o
+v.gsflow.segments input=$streams_onebasin output=$segments icalc=$icalc --o
 
 # MODFLOW grid & basin mask (1s where basin exists and 0 where it doesn't)
 v.gsflow.grid basin=$basins_onebasin dx=$grid_res dy=$grid_res output=$grid mask_output=$basin_mask pour_point=$pour_point --o
@@ -68,11 +70,15 @@ v.gsflow.gravres hru_input=$HRUs grid_input=$grid output=$gravity_reservoirs --o
 
 # Export DEM with MODFLOW resolution
 # Also export basin mask -- 1s where basin exists and 0 where it doesn't
-g.region rast=$basin_mask res=$grid_res
+v.to.rast in=$basins_onebasin out=$basin_mask use=val val=1 --o
+g.region vect=$grid res=$grid_res
+r.resamp.stats in=$basin_mask out=$basin_mask method=sum --o
+r.mapcalc "$basin_mask = $basin_mask > 0" --o
+#g.region rast=$DEM
 r.out.ascii input=$DEM output=$DEM.asc null_value=0 --o
 r.out.ascii input=$basin_mask output=$basin_mask_out.asc null_value=0 --o
 g.region rast=$DEM
-g.region vect=$basins_onebasin
+#g.region vect=$basins_onebasin
 
 # Export tables and discharge point
 v.gsflow.export reaches_input=$reaches segments_input=$segments gravres_input=$gravity_reservoirs hru_input=$HRUs pour_point_input=$pour_point reaches_output=$reaches segments_output=$segments gravres_output=$gravity_reservoirs hru_output=$HRUs pour_point_output=$pour_point --o
