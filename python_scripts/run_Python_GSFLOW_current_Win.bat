de3748688506e4ce995148cf3a758576644718fb
Crystal Ng <gcng@umn.edu>
2017-10-28 18:41:48 -0500

    other edits

--- python_scripts/run_Python_GSFLOW_current_Win.bat
+++ python_scripts/run_Python_GSFLOW_current_Win.bat
@@ -1,17 +1,18 @@
 
-preproc_dir="/home/gcng/workspace/matlab_files/GSFLOW_pre-processor/python_scripts/"
-input_file="/home/gcng/workspace/matlab_files/GSFLOW_pre-processor/python_scripts/settings_new.ini"
+SET preproc_dir=D:\GSFLOW\Pre_processor_codes\Shullcas_5\python_scripts\
+SET input_file=D:\GSFLOW\Pre_processor_codes\Shullcas_5\python_scripts\settings_new.ini
+SET python2.7=C:\Users\lsomers\Anaconda2\python.exe
 
 :: ----------------------------------------------------------------------------
 :: (Do not change below this line)
 copy %input_file% settings.ini
 
-python2.7 %preproc_dir%settings_test.py 
-python2.7 %preproc_dir%Create_hydcond_array.py
-python2.7 %preproc_dir%GSFLOW_print_data_climatehru_files1.py
-python2.7 %preproc_dir%GSFLOW_print_controlfile_current.py
-python2.7 %preproc_dir%GSFLOW_print_PRMSparamfile_current.py
-python2.7 %preproc_dir%print_MODFLOW_inputs_res_NWT_current.py
-python2.7 %preproc_dir%run_GSFLOW.py
-
+%python2.7% %preproc_dir%settings_test.py 
+%python2.7% %preproc_dir%Create_hydcond_array.py
+%python2.7% %preproc_dir%GSFLOW_print_data_climatehru_files1.py
+%python2.7% %preproc_dir%GSFLOW_print_controlfile_current.py
+%python2.7% %preproc_dir%GSFLOW_print_PRMSparamfile_current.py
+%python2.7% %preproc_dir%print_MODFLOW_inputs_res_NWT_current.py
+%python2.7% %preproc_dir%run_GSFLOW.py
+pause
 
